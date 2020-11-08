9546175f474584b715fdf0c4a41798268e96ea9a
makredzic <mak.redzic@gmail.com>
2020-11-08 11:14:27 +0100

    Init commit

---
+++ zadatak1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+//Welcome to Unit converter. Please enter one of the following options:
+//1. Temperature
+//2. Speed
+//3. Length
+//4. Weight
+//5. Fuel economy
+//Your choice:
+
+int main() {
+
+  unsigned short choice;
+
+  std::cout<<"Welcome to Unit converter. Please enter one of the following options:\n\t1. Temperature\n\t2. Speed\n\t3. Length\n\t4. Weight\n\t5. Fuel economy\nYour choice: ";
+  std::cin>>choice;
+
+  return 0;
+}
