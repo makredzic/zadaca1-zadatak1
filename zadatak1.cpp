c2e54a67fb0e0b3030db7bc4bd110d1daf33022c
makredzic <mak.redzic@gmail.com>
2020-11-08 12:17:15 +0100

    Removed while loop logic. Added Menu namespace and functions for menus

--- zadatak1.cpp
+++ zadatak1.cpp
@@ -1,35 +1,31 @@
 #include <iostream>
-using namespace std;
 
-//Welcome to Unit converter. Please enter one of the following options:
-//1. Temperature
-//2. Speed
-//3. Length
-//4. Weight
-//5. Fuel economy
-//Your choice:
+namespace Menu {
+  void main_menu() {
+    std::cout<<"Welcome to the Unit converter. Please enter one of the following options:\n\t1. Temperature\n\t2. Speed\n\t3. Length\n\t4. Weight\n\t5. Fuel economy\n";
+  };
+
+  void temperature_menu() {
+    std::cout<<"Please choose the converter:\n\t1. Celsius to Fahrenheit\n\t2. Fahrenheit to Celsius\n\t3. Celsius to Kelvin\n\t4. Kelvin to Celsius\n\t5. Fahrenheit to Kelvin\n\t6. Kelvin to Fahrenheit\n";
+  }
+
+}
+
 
 int main() {
+  using namespace Menu;
+  using std::string;
 
   unsigned short choice;
-  bool check;
 
-  std::cout<<"Welcome to Unit converter. Please enter one of the following options:\n\t1. Temperature\n\t2. Speed\n\t3. Length\n\t4. Weight\n\t5. Fuel economy\n";
-  do {  
-    check = true;
-    std::cout<<"Your choice: ";
-    std::cin>>choice;
+  main_menu();
+  std::cout<<"Your choice: ";
+  std::cin>>choice;
 
-    if(choice != 1 && choice != 2 && choice != 3 && choice != 4 && choice != 5) {
-      check = false;
-      std::cout<<"Please type the appropriate number.\n";
-    }
-
-  } while(!check);
-
+  if (choice == 1) {
+    temperature_menu();
+  } else std::cout<<"Not implemented yet.\n";
+  
   std::cout<<"End of program!\n";
-  
-  
-  
   return 0;
 }
