6d6039557bd7c831a67a93c69c234105e21de223
makredzic <mak.redzic@gmail.com>
2020-11-08 11:48:14 +0100

    Added while loop to check choice

--- zadatak1.cpp
+++ zadatak1.cpp
@@ -12,9 +12,24 @@ using namespace std;
 int main() {
 
   unsigned short choice;
+  bool check;
 
-  std::cout<<"Welcome to Unit converter. Please enter one of the following options:\n\t1. Temperature\n\t2. Speed\n\t3. Length\n\t4. Weight\n\t5. Fuel economy\nYour choice: ";
-  std::cin>>choice;
+  std::cout<<"Welcome to Unit converter. Please enter one of the following options:\n\t1. Temperature\n\t2. Speed\n\t3. Length\n\t4. Weight\n\t5. Fuel economy\n";
+  do {  
+    check = true;
+    std::cout<<"Your choice: ";
+    std::cin>>choice;
 
+    if(choice != 1 && choice != 2 && choice != 3 && choice != 4 && choice != 5) {
+      check = false;
+      std::cout<<"Please type the appropriate number.\n";
+    }
+
+  } while(!check);
+
+  std::cout<<"End of program!\n";
+  
+  
+  
   return 0;
 }
