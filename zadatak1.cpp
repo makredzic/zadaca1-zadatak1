329ed334c2de62946b6bc0e742eb993e4a0f4d42
makredzic <mak.redzic@gmail.com>
2020-11-08 12:35:30 +0100

    Added while logic but with menu functions. Added 'get_choice()' function.

--- zadatak1.cpp
+++ zadatak1.cpp
@@ -9,6 +9,12 @@ namespace Menu {
     std::cout<<"Please choose the converter:\n\t1. Celsius to Fahrenheit\n\t2. Fahrenheit to Celsius\n\t3. Celsius to Kelvin\n\t4. Kelvin to Celsius\n\t5. Fahrenheit to Kelvin\n\t6. Kelvin to Fahrenheit\n";
   }
 
+  int get_choice() {
+    std::cout<<"Your choice: ";
+    unsigned short x;
+    std::cin>>x;
+    return x;
+  }
 }
 
 
@@ -16,16 +22,38 @@ int main() {
   using namespace Menu;
   using std::string;
 
+  bool to_loop;
   unsigned short choice;
 
   main_menu();
-  std::cout<<"Your choice: ";
-  std::cin>>choice;
+  do {
+    to_loop = false;
+    choice = get_choice();
 
-  if (choice == 1) {
-    temperature_menu();
-  } else std::cout<<"Not implemented yet.\n";
+      if (choice == 1) {
+      temperature_menu();
+    } else 
+    
+    if (choice == 2) {
+      std::cout<<"Not implemented yet.\n";
+    } else 
   
+    if (choice == 3) {
+      std::cout<<"Not implemented yet.\n";
+    } else
+
+    if (choice == 4) {
+      std::cout<<"Not implemented yet.\n";
+    } else  
+  
+    if (choice == 5) {
+      std::cout<<"Not implemented yet.\n";
+    } else {
+      std::cout<<"Wrong choice.\n";
+      to_loop = true;
+    }
+  } while (to_loop);
+
   std::cout<<"End of program!\n";
   return 0;
 }
