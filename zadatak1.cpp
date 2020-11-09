4c59fa0b0a3db8e78442868979576287c2ee89d5
makredzic <mak.redzic@gmail.com>
2020-11-09 16:27:37 +0100

    Added namespace with functions for all temperature conversions

--- zadatak1.cpp
+++ zadatak1.cpp
@@ -33,11 +33,100 @@ namespace Menu {
   }
 }
 
+
+//temperature converters
+namespace t_con {
+  float cels_fahr(float cels) {
+    double result = 1.8*cels + 32;
+    return result;
+  }
+
+  float fahr_cels(float fahr) {
+    float result = (fahr-32)/1.8;
+    return result;
+  }
+
+  float cels_kelv(float cels) {
+    return cels+273.15;
+  }
+
+  float kelv_cels(float kelv) {
+    return kelv-273.15;
+  }
+
+  float fahr_kelv(float fahr) {
+    float result = (fahr-32)/1.8 + 273.15;
+    return result;
+  }
+
+  float kelv_fahr(float kelv) {
+    float result = (kelv-273.15)*1.8 + 32 + 273.15;
+    return result;
+  }
+}
+
+
 namespace MenuManager {
+  
   using namespace Menu;
+  using namespace t_con;
+  
   void call_menu(unsigned short x) {
+    
     if (x == 1) {
       temperature_menu();
+      unsigned short choice = get_choice();
+     
+      if (choice == 1) {
+        float c;
+        do{
+          std::cout<<"Enter the temperature in celsius: ";
+          std::cin>>c;
+        
+          if (c < -273.15) {
+            std::cout<<"Too low (<273.15c).\n";
+          }
+        } while (c < -273.15);
+        std::cout<<c<<" degrees celsius is "<<cels_fahr(c)<<" degrees fahrenheit.\n";
+      } else
+
+      if (choice == 2) {
+        float f;
+        do{
+          std::cout<<"Enter the temperature in degrees Fahrenheits: ";
+          std::cin>>f; 
+          if (f < -459.67) {
+            std::cout<<"Too low (<-459.67).\n";
+          }
+
+        } while (f < -459.67);
+        std::cout<<f<<" degrees Fahrenheits is "<<fahr_cels(f)<<" degrees celsius.\n";
+      } else 
+
+      if (choice == 3) {
+        float c;
+        do{
+          std::cout<<"Enter the temperature in degrees celsius: ";
+          std::cin>>c; 
+          if (c < -273.15) {
+            std::cout<<"Too low (< -273.15).\n";
+          }
+        } while (c < -273.15);
+        std::cout<<c<<" degrees Celsius is "<<cels_kelv(c)<<" Kelvin.\n";
+      } else 
+
+      if (choice == 4) {
+        float k;
+        do{
+          std::cout<<"Enter the temperature in kelvin: ";
+          std::cin>>k; 
+          if (k < 0) {
+            std::cout<<"Too low (<0).\n";
+          }
+        } while (k < 0);
+        std::cout<<k<<" kelvin is "<<kelv_cels(k)<<" degrees Celsius.\n";
+      } 
+     
     } else
 
     if (x == 2) {
@@ -57,7 +146,6 @@ namespace MenuManager {
     }
   }
 }
-
 
 int main() {
   using namespace Menu;
